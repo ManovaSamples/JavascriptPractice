4f579946f0f4291064b96dabe0f73cee639aa2bb
Manova Chandramohan <manova.chandramohan.ext@zeiss.com>
2023-10-18 15:43:00 +0530

    18-Oct-23 	modified:   freecodecamp/basics.js

--- freecodecamp/basics.js
+++ freecodecamp/basics.js
@@ -4,3 +4,47 @@
 line
 comment
 */
+
+//Declare
+var ourName;
+
+//Store value
+myVariable = 5;
+var myVar;
+myVar = 5;
+
+//Assigning the value
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+// console.log(myNum)
+
+//Initialize the variable
+var myNumber = 9;
+
+//Declare strings
+var myName = 'My name is Manova'
+// console.log(myName)
+
+//concat
+var a = 5
+var b  = 10
+var c = "I am a"
+
+// console.log(a+1)
+// console.log(b+5)
+// console.log(c+" String!")
+
+// ***** always decalre variable name in camelCase
+
+//Difference between let and var
+var myFirstName = "Manova"
+var myFirstName = "CM"
+console.log(myFirstName)
+
+// let myNameBe = "Manova"
+// let myNameBe = "CM"
+// console.log(myNameBe)
+//SyntaxError: Identifier 'myNameBe' has already been declared
+
